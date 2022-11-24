2ec8c24394972a99220e852ff0faaca3b51df403
James Tiger <jmepaz@gmail.com>
2022-11-25 00:23:00 +0800

    Restoration of Image
    Able to restore image in website interface

--- flask-server-venv/ImageProcessing.py
+++ flask-server-venv/ImageProcessing.py
@@ -5,6 +5,7 @@ import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 import piexif as p
+import sys
 
 from skimage import color
 def conv2Img(base64_enc: str):
@@ -41,6 +42,32 @@ def kernel_tostr(kernel: np.ndarray):
       lst.append(str_arr[1:-1].strip())
     return "\n".join(lst)
 
+def str_to_kernel(str_arr):
+    data_lst = []
+    for elem in str_arr.split('\n'):
+      curr_lst = list(map(float, elem.strip().split()))
+      data_lst.append(curr_lst)
+    return np.array(data_lst)
+
+def extract_exifData(img):
+    if ('exif' not in img.info):
+        return (None, None)
+
+    exif_info = p.load(img.info['exif'])
+    if('Exif' not in exif_info):
+        return (None, None)
+    elif 37510 not in exif_info['Exif'] :
+        return (None, None)
+    
+    raw_data = exif_info['Exif'][37510].decode("utf-8").split("@\n")
+    
+    try:
+        filter_type = raw_data[0]
+        kernel = str_to_kernel(raw_data[1])
+    except Exception as e:
+        return (None, None)
+
+    return (filter_type, kernel)
 
 """
    Convolutional Filter
@@ -94,4 +121,22 @@ def applyFilter(imgB64, spec_k):
     img = FilterImg.conv_filter(np.array(img)/1.0, spec_k)
     img = Image.fromarray(_asUInt8(img))
     return conv2B64(img)
+
+from skimage import img_as_float
+from skimage import restoration
+rng = np.random.default_rng()
+
+def restore_img(img, spec_kernel):
+    img += 0.1 * img.std() * rng.standard_normal(img.shape)
+    deconvolved, stat = restoration.unsupervised_wiener(img, spec_kernel)
+    return deconvolved
+
+def image_restoration(img, spec_kernel):
+    img = np.asarray(img)
+    img = clip_color(img, 0, 1)
+    img = img_as_float(img)
+    restored = restore_img(img, spec_kernel) 
+    restored = clip_color(restored, 0, 1)
+    restored_img = Image.fromarray(_asUInt8(restored)) 
+    return restored_img
 #=============================================
--- react-app/src/RestorationPage.js
+++ react-app/src/RestorationPage.js
@@ -1,9 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
 import React, { useState, useEffect } from 'react';
+import piexif from '../node_modules/piexifjs/piexif.js';
 function RestorationPage() {
   const [prevImg, updatePrevImg] = useState(()=>{return logo;});
-
+  const [restoredImg, updateRestImg] = useState(()=>{})
+  const [filterType, updateFilterType] = useState(()=>{return "";});
+  const [kernel, updateKernel] = useState(()=>{return "";})
+  
+  var loading = undefined;
+  var kernelInLoad = undefined;
   function setBase64Img(file){
     var fr = new FileReader();
     fr.readAsDataURL(file);
@@ -29,6 +35,79 @@ function RestorationPage() {
     setBase64Img(file);
    
   }
+  function getImgExifData(imgB64){
+    try{
+      var exifObj = piexif.load(imgB64);
+      if('Exif' in exifObj && 37510 in exifObj['Exif']){
+        var data = exifObj['Exif'][37510].split(/@\r?\n/gm);
+        kernelInLoad = data[1];
+        updateFilterType(data[0])
+        updateKernel(data[1])
+        return data
+      }
+      else{
+        console.log('Not Detected')
+      }
+    }
+    catch(err){
+      console.log('Error')
+    }
+
+  }
+
+   useEffect(() => {
+    if(prevImg!=logo){
+     restoreImgRequest()
+}}, [prevImg])
+
+  const restoreImgRequest = () => {
+      // if loading = undefined
+      if(loading === undefined){
+          loading = document.getElementById("loading-img");
+      }
+      loading.style.visibility = "visible";
+
+       //data required
+       const imgB64 = prevImg.replace(/^data:image\/\D+;base64,/gm, "");
+       const exifData = getImgExifData(prevImg);
+       const primaryData = {'img':imgB64, 'kernel': exifData[1]}
+    //     //fetching
+      fetch("http://localhost:5000/restorationRequest", {
+        method: 'POST', // *GET, POST, PUT, DELETE, etc.
+        mode: 'cors', // no-cors, *cors, same-origin
+        cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+        headers: {
+          'Content-Type': 'application/json'
+        },
+          body: JSON.stringify(primaryData) // body data type must match "Content-Type" header
+      })
+      .then(
+        res => res.json()
+      ).then(
+        data => {
+
+          if(data['status']==true){
+            // // the kernel used 
+            // //console.log('Kernel Used:\n'+data['spec-kernel']) // [For debug]
+
+            // // save kernel
+            // var newImg = saveKernelonImg(data['spec-kernel'], "data:image/jpeg;base64,"+data["mod-img"])
+
+            // // this set to the preview image
+            // updatePrevImg(newImg)
+            updateRestImg("data:image/jpeg;base64,"+data['restored-img'])
+          }
+          else{
+            alert(data['remarks'])
+          }
+          console.log(data)
+        }
+      ).then(
+        res =>{
+          loading.style.visibility = "hidden";
+        }
+    )
+  }
   return (
     <div className="flex w-[95%] h-full mx-auto mt-5">
       <div className="w-1/2 flex flex-col items-center">
@@ -38,10 +117,6 @@ function RestorationPage() {
           </div>
           <input type="file" className= "p-4 border  mt-5 rounded w-[20em] max-h-[40px] py-[6px]" accept=".jpg,.jpeg" onChange={getImgFile}/>
           <div className="h-[25em] w-[35em] mt-5 bg-slate-200 rounded flex flex-col items-center">
-            <div id="loading-img" width="95%" height="100%" className="invisible absolute z-10 top-[55%]  flex items-center mx-auto bg-slate-700/80 rounded px-5 py-3" >
-              <img src={logo} width="70px" height="70px" className="animate-spin" alt="loading screen"/>
-              <p className="text-white font-bold">Loading...</p>
-            </div>
             <img id="prev-img" src={prevImg}  className="mx-auto h-full w-auto"/>
           </div>
         </div>
@@ -58,12 +133,13 @@ function RestorationPage() {
               <img src={logo} width="70px" height="70px" className="animate-spin" alt="loading screen"/>
               <p className="text-white font-bold">Restoring...</p>
             </div>
-            <img id="prev-img" src={logo} width="95%" className="mx-auto h-full w-auto"/>
+            <img id="restored-img" src={restoredImg} width="95%" className="mx-auto h-full w-auto"/>
           </div>
           <div className="w-full my-4">
             <button className="p-4 border bg-green-700 rounded text-white py-[8px]"> Download Restored Img </button>
-            <p>Detected Filter Type: Gaussian Blur</p>
-            <p>Detected Kernel Used:</p>
+            <p><span className="font-bold">Detected Filter Type:</span> {filterType}</p>
+            <p className="font-bold">Detected Kernel Used:</p>
+            <p>{kernel}</p>
           </div>
         </div>
       </div>
